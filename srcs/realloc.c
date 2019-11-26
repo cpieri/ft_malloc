b4824e4ee5ad938df5b347be1aabfcdb8bbd1173
Louise P <cpieri@student.42.fr>
2019-11-26 11:06:15 +0100

    [Debug]: All programs run Vi, Bash, Sh, Ls,.....

--- srcs/realloc.c
+++ srcs/realloc.c
@@ -6,7 +6,7 @@
 /*   By: cpieri <cpieri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/21 11:33:06 by cpieri            #+#    #+#             */
-/*   Updated: 2019/11/26 10:56:28 by cpieri           ###   ########.fr       */
+/*   Updated: 2019/11/26 11:05:27 by cpieri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,8 @@ void	*realloc(void *ptr, size_t size)
 	void	*new_ptr;
 	t_block	*block;
 
-	// ft_putstr("\033[38;5;206m-- Enter in realloc --\033[0m\n");
-	// ft_putstr("\033[32mptr = 0x"), ft_put64hexa((uint64_t)&*ptr), ft_putstr("\033[0m\n");
 	if (!size)
-	{
-		// ft_putstr("\033[35m-- Out of realloc: NULL --\033[0m\n");
-		return (NULL);
-	}
+		size = ROUNDUP;
 	if ((block = check_if_block_exist(ptr)) != NULL)
 	{
 
@@ -31,11 +26,9 @@ void	*realloc(void *ptr, size_t size)
 			return (NULL);
 		new_ptr = ft_memcpy(new_ptr, ptr, block->size);
 		free(ptr);
-		// ft_putstr("\033[35m-- Out of realloc --\033[0m\n");
 		return (new_ptr);
 	}
 	if ((new_ptr = malloc(size)) == NULL)
 		return (NULL);
-	// ft_putstr("\033[35m-- Out of realloc: NULL --\033[0m\n");
 	return (new_ptr);
 }
