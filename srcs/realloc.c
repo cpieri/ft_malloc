2e5ce2020a2b9631181236c02fc356b82cb46950
Louise P <cpieri@student.42.fr>
2019-11-26 11:53:59 +0100

    [Debug]: zsh

--- srcs/realloc.c
+++ srcs/realloc.c
@@ -6,7 +6,7 @@
 /*   By: cpieri <cpieri@student.le-101.fr>          +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/11/21 11:33:06 by cpieri       #+#   ##    ##    #+#       */
-/*   Updated: 2019/11/26 11:14:52 by cpieri      ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/11/26 11:52:54 by cpieri      ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -16,6 +16,7 @@
 void	*realloc(void *ptr, size_t size)
 {
 	void	*new_ptr;
+	size_t	size_cpy;
 	t_block	*block;
 
 	if (!size)
@@ -24,7 +25,8 @@ void	*realloc(void *ptr, size_t size)
 	{
 		if ((new_ptr = malloc(size)) == NULL)
 			return (NULL);
-		new_ptr = ft_memcpy(new_ptr, ptr, block->size);
+		size_cpy = (size < block->size) ? size : block->size;
+		new_ptr = ft_memcpy(new_ptr, ptr, size_cpy);
 		free(ptr);
 		return (new_ptr);
 	}
